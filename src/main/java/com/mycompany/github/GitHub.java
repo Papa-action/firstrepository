5ec244578a71f25cd9b4ba3dacd9f9d0e81a190d
User <User@10.76.233.58>
2024-09-26 17:02:27 +0200

    third commit

--- src/main/java/com/mycompany/github/GitHub.java
+++ src/main/java/com/mycompany/github/GitHub.java
@@ -10,9 +10,32 @@ package com.mycompany.github;
  */
 public class GitHub {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.print("enter fullname ");
-        
+            boolean checkUserName(String Name){
+         if (Name == null) {
+            return false; // Return false if username does not contain an underscore and more than 5 characters long
+        }
+        //Reurn true if Name contains undersore and is no more than 5 characters long  
+        return Name.contains("_")&& Name.length()<=5; 
+    }  
+
+            boolean checkPasswordComplexity(String password){
+         if (password == null) {
+            return false; // Return false if the password does not meet the conditions
+        }
+        // Return true if password contains atleast 8 characters,a capital letter, a number and a special character 
+        return password.length()>=8&& password.matches(".*[A-Z].*")&& password.matches(".*\\d.*")
+                && password.contains("@")
+                || password.contains("!")                
+                || password.contains("#") 
+                || password.contains("%")
+                || password.contains("$")
+                || password.contains("&")
+                || password.contains("*")
+                || password.contains("?");
     }
+
+
+
+
+
 }
