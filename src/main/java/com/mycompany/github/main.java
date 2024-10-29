2130704e29f55005d9fdb1e4071808c5bb57e67e
User <User@10.76.233.58>
2024-10-29 15:35:48 +0200

    commit

--- src/main/java/com/mycompany/github/main.java
+++ src/main/java/com/mycompany/github/main.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.github;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 /**
  *
  * @author User
@@ -11,10 +12,21 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
                 Scanner input = new Scanner (System.in);
-        GitHub checker = new GitHub();
+        Login checker = new Login();
         String firstName = "Yonela";
         String LastName = "Nuba";
+        String name ="";
+        String lastName="";
+        String taskName="";
+        String taskDescription="";
+        int a;
+        int b = 0;
+        String num1;
+        int taskNumber = -1;
+        int hours = 0;
+        int addhours = 0 ;
         
+
         System.out.println("enter username");
         String Name = input.next();
         
@@ -59,5 +71,95 @@ public class main {
         }
         System.out.println(checker.returnLoginStatus(createPassword, createUsername, Name, password));
    
+         Task currentTask = new Task(name, lastName, taskName, taskDescription);      
+       
+        String welcome = "Welcome to EasyKanban";
+        
+        JOptionPane.showMessageDialog(null, welcome);
+        
+        
+
+        while (true) {
+            
+            String choicestr = JOptionPane.showInputDialog(null,
+                    "1. Add task\n"
+                    + "2. Show report\n"
+                    + "3. Quit"
+            );
+
+            
+            int choice = Integer.parseInt(choicestr);
+
+            
+            switch (choice) {
+                case 1:
+                    
+                    JOptionPane.showMessageDialog(null, "You selected: Add task");
+
+                    
+                    num1 = JOptionPane.showInputDialog(null, "Select the number of tasks to be added:");
+                    a = Integer.parseInt(num1);
+
+                    
+                    while (b < a) {
+                        b++;  
+                
+                        name = JOptionPane.showInputDialog(null, "Enter first name:");
+                        lastName = JOptionPane.showInputDialog(null, "Enter last name:");
+                        taskName = JOptionPane.showInputDialog(null, "Enter task name:");
+                        taskDescription = JOptionPane.showInputDialog(null, "Enter task description:");
+                        
+                    String c = JOptionPane.showInputDialog(null, "enter duration of task ");
+                    addhours = Integer.parseInt(c);
+                
+                    hours  = hours + addhours;
+                        
+                        while (!(currentTask.checkTaskDescription(taskDescription ))) {
+                            taskDescription = JOptionPane.showInputDialog(null, "Task description must be less than 50 characters long.");
+                        }
+
+                        
+                        JOptionPane.showMessageDialog(null,
+                                currentTask.printTaskDetails(name, lastName, taskName, taskDescription)
+                                + "\n " + currentTask.TaskID(taskName, name)
+                                +"\n Duration:"+addhours);
+                        
+                        
+                  }  
+                       JOptionPane.showMessageDialog(null,"Total hours:" + currentTask.returnTotalHours(hours));
+                    
+                    
+                    break;
+
+                case 2:
+                   
+                    JOptionPane.showMessageDialog(null, "Coming soon");
+                    
+                    break;
+
+                case 3:
+                    
+                    JOptionPane.showMessageDialog(null, "You selected: Quit");
+                    System.exit(0);  // Exit the program
+                    break;
+
+                default:
+                    
+                    JOptionPane.showMessageDialog(null, "Invalid choice! Please select 1, 2, or 3.");
+                    break;
+            }
+        
+
+                choicestr = JOptionPane.showInputDialog(null, "Choose an option:\n1. To Do\n2. Doing\n3. Done");
+
+               
+                choice = Integer.parseInt(choicestr);
+
+               
+                
+                
+
+        } 
+    
     }   
 }
